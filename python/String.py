e7dfc524a9923fc652212c55e4005acd314e97a4
ocder <m17364526124@163.com>
2018-10-30 20:56:35 +0800

    为了美赛，努力学习

---
+++ python/String.py
@@ -0,0 +1,10 @@
+str = 'Runoob'
+print(str)
+print(str[0 : -1])  #这里的-1不包括最后一位
+print(str[0])
+print(str[2 : 5])
+print(str[2 : ])
+print(str * 2)
+print(str + '你好')
+print('hello\nrunoob')
+print(r'hello\nrunoob')
