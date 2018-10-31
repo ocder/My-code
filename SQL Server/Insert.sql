a44e67cd5b7d61138c49717c865cb94652a6e6f8
ocder <m17364526124@163.com>
2018-10-31 16:59:39 +0800

    向数据库中插入数据

---
+++ SQL Server/Insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO Employee VALUES('1002', '胡一民', '男', '38', '1', '工程师', '01');
+INSERT INTO Employee VALUES('1004', '王爱民', '男', '60', '1', '高工', '03');
+INSERT INTO Employee VALUES('1005', '张小华', '女', '50', '1', '工程师', '02');
+INSERT INTO Employee VALUES('1010', '宋文彬', '男', '36', '1', '工程师', '01');
+INSERT INTO Employee VALUES('1011', '胡民', '男', '34', '1', '工程师', '01');
+INSERT INTO Employee VALUES('1015', '黄晓英', '女', '26', '1', '助工', '03');
+INSERT INTO Employee VALUES('1022', '李红卫', '女', '27', '0', '助工', '02');
+INSERT INTO Employee VALUES('1031', '丁为国', '男', '24', '0', '助工', '02');
