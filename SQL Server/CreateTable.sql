ac513823c36b0b7678759569727d82c814c4385b
ocder <m17364526124@163.com>
2018-11-01 22:01:29 +0800

    血泪史，什么也不说了，从删库到跑路终于用java连上了SQL Server 2017

--- SQL Server/CreateTable.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Employee
-(Eno        CHAR(4)NOT NULL UNIQUE,
-Ename       CHAR(8)NOT NULL,
-Sex         CHAR(2)NOT NULL DEFAULT('男'),
-Age         INT NULL,
-Is_Marry    CHAR(1)NULL,
-Title       CHAR(6)NULL,
-Dno         CHAR(2)NULL);
