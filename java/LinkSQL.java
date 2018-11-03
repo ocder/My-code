d20ab5adc6545776c089ea4517d73b48dc6df50c
ocder <m17364526124@163.com>
2018-11-03 12:58:11 +0800

    优化了代码的结构，封装了数据库连接和关闭接口

--- java/LinkSQL.java
+++ java/LinkSQL.java
@@ -1,27 +1,45 @@
 import java.sql.*;
+import java.util.*;
 
 public class LinkSQL {
 
-    public static void main(String[] args) {
-      GetConnection();
-  }
-  /*和数据库取得连接函数*/
-    public static Connection GetConnection() {
-        String driverName="com.microsoft.sqlserver.jdbc.SQLServerDriver";//SQL数据库引擎
-        String dbURL="jdbc:sqlserver://localhost:1433;DatabaseName=Test";//数据源  ！！！！注意若出现加载或者连接数据库失败一般是这里出现问题
-        String Name="sa";
-        String Pwd="wqshd16073123";
-        Connection conn = null;
+    private static final String driverName="com.microsoft.sqlserver.jdbc.SQLServerDriver";//SQL数据库引擎
+    private final static String dbURL="jdbc:sqlserver://localhost:1433;DatabaseName=Test";//数据源  ！！！！注意若出现加载或者连接数据库失败一般是这里出现问题
+    private static final String Name="sa";
+    private static final String Pwd="wqshd16073123";
 
-        try{
+    private static Connection conn = null;
+    //静态代码块（将加载驱动、连接数据库放入静态块中）
+    static {
+        try {
             Class.forName(driverName);
             conn=DriverManager.getConnection(dbURL,Name,Pwd);
             System.out.println("连接数据库成功");
         } 
-        catch(Exception e){
+        catch(Exception e) {
             e.printStackTrace();
             System.out.println("连接失败");
         }
+    }
+    //对外提供一个方法来获取数据库连接
+    public static Connection getConnection(){
         return conn;
+    }
+    //对外提供一个方法来关闭数据库连接
+    public static void closeConnection() {
+        if(conn != null) {  // 关闭连接对象   
+            try {   
+               conn.close();
+               System.out.println("数据库已关闭");
+            }
+            catch(SQLException e) {   
+               e.printStackTrace();   
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        getConnection();
+        closeConnection();
     }
 }
