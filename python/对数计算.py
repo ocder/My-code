7be2440b686a1493c837c56090f8d31647a9da33
ocder <m17364526124@163.com>
2018-12-05 19:46:55 +0800

    计算熵时有负号还有概率

--- python/对数计算.py
+++ python/对数计算.py
@@ -4,7 +4,6 @@ posi = [0.20, 0.19, 0.18, 0.17, 0.15, 0.10, 0.01]
 sum = 0
 for item in posi:
     # 以二为底
-    sum = math.log(item, 2) + sum
-    print(math.log(item, 2))
+    sum = -(item * math.log(item, 2)) + sum
 
 print(sum)
